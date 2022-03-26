50f791cdfe2bd6e79fcdf30a4eb03ea3599606cf
atil <hyperion91@gmail.com>
2022-03-26 12:54:08 +0100

    Fix world shader vertex attribute locations
    Apparently, different drivers may sort them arbitrarily. We need to provide explicit layout locations

--- src/world.glsl
+++ src/world.glsl
@@ -1,6 +1,7 @@
 #ifdef VERTEX
-in vec3 in_pos;
-in vec2 in_texcoord;
+
+layout(location = 0) in vec3 in_pos;
+layout(location = 1) in vec2 in_texcoord;
 
 uniform mat4 u_model;
 uniform mat4 u_view;
