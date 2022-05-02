4ec1eaad9375cc0829fadcdff0f99e8b1203e564
atil <hyperion91@gmail.com>
2022-05-03 00:53:46 +0200

    Add method stub

--- src/core.h
+++ src/core.h
@@ -1,3 +1,8 @@
+struct EntityId {
+    EntityIndex index;
+    String tag;
+};
+
 struct Core {
     Array<struct GameObject> go_data;
     Array<struct GoRenderUnit> go_render;
@@ -6,6 +11,8 @@ struct Core {
     Array<struct Widget> ui_widgets;
     Array<struct UiRenderUnit> ui_render;
 
+    Array<EntityId> entities; // This is gonna replace PongEntities
+
     Core() : go_data(10), go_render(10), particle_sources(10), particle_render(10), ui_widgets(10), ui_render(10) {
     }
 
@@ -13,5 +20,7 @@ struct Core {
     Core(Core &&) = delete;
     Core &operator=(const Core &) = delete;
     Core &operator=(const Core &&) = delete;
+
+    EntityIndex get_entity_by_tag(String *tag);
 };
 
